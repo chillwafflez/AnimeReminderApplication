7b86d5566ae76e1eee422ccff11b27e13d28d897
Justin Nguyen <ju5t1n.nguy3n284@gmail.com>
2023-01-19 21:52:14 -0800

    added schedule stuff to send windows noti once it hits a certain time

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fetchMethods import *
 from win10toast import ToastNotifier
+import schedule
+import time
+from datetime import datetime
 
 
-#toaster = ToastNotifier()
-#toaster.show_toast("Wsup", "cock", duration=3)
+
+toaster = ToastNotifier()
+toaster.show_toast("Wsup", "cock", duration=3)
 
 #print(searchAnime("Attack on Titan"))
 data = getNextEpisode("Vinland Saga Season 2")
@@ -11,3 +15,4 @@ airingAt = data['data']['Media']['nextAiringEpisode']['airingAt']
 timeUntilAiring = data['data']['Media']['nextAiringEpisode']['timeUntilAiring']
 print(airingAt)
 print(timeUntilAiring)
+
--- testTime.py
+++ testTime.py
@@ -1,3 +1,34 @@
-stopTimestamp = 1674095400
+from datetime import datetime
+import time
+import schedule
+from win10toast import ToastNotifier
+import threading
 
 
+#currentTimestamp = 167419122
+#print(time.ctime(currentTimestamp))
+
+running = True
+def wsup():
+    global running
+    running = False
+    toaster = ToastNotifier()
+    toaster.show_toast("Wsup", "cock", duration=3)
+    print("you should see this at 9:45")
+    return schedule.CancelJob
+
+
+#3 mins from now
+currentTimestamp = 1674193711
+format = "%H:%M"
+t = datetime.fromtimestamp(currentTimestamp)
+stopTimestamp = t.strftime(format)
+
+
+
+schedule.every().day.at(stopTimestamp).do(wsup)
+while running:
+    schedule.run_pending()
+    time.sleep(1)
+
+print("end")
