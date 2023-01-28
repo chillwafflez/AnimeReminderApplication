9f6f60b37bd91c59a3b1a6a6e7a0e4f1dfa19d4c
Justin Nguyen <ju5t1n.nguy3n284@gmail.com>
2023-01-28 04:17:20 -0800

    added looping through data of anime list fetched

--- test.py
+++ test.py
@@ -2,7 +2,18 @@ from fetchMethods import *
 
 data = getWatchList("chillwafflez")
 #this gets gurren lagann's "media" fields/info
-print(data["data"]['MediaListCollection']['lists'][0].get('entries')[0].get('media'))
+#print(data["data"]['MediaListCollection']['lists'][0].get('entries')[0].get('media'))
 
 #exp: this gets gurren lagann's status (finished)
-print(data["data"]['MediaListCollection']['lists'][0].get('entries')[0].get('media').get('status'))
+#print(data["data"]['MediaListCollection']['lists'][0].get('entries')[0].get('media').get('status'))
+
+
+animeListNAMES = []
+
+for i in range(len(data["data"]['MediaListCollection']['lists'][0].get('entries'))):
+
+    if (data["data"]['MediaListCollection']['lists'][0].get('entries')[i].get('media').get('status') == 'RELEASING'):
+        animeListNAMES.append(data["data"]['MediaListCollection']['lists'][0].get('entries')[i].get('media').get('title').get('english'))
+
+
+print(animeListNAMES)
